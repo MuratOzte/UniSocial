01861ce2ca0b0a8dfe09caa4506a122d4b3be1fd
MuratOzte <mo2553481@gmail.com>
2024-10-26 20:45:43 +0300

    uiSlice added

---
+++ src/store/Slices/uiSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const registerSlice = createSlice({
+    name: 'ui',
+    initialState: {},
+    reducers: {},
+});
+
+export default registerSlice;
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+//slices
 import registerSlice from './Slices/RegisterSlice';
 import loginSlice from './Slices/LoginSlice';
+import uiSlice from './Slices/uiSlice';
 
 export const store = configureStore({
     reducer: {
         register: registerSlice.reducer,
         login: loginSlice.reducer,
+        ui: uiSlice.reducer,
     },
 });
