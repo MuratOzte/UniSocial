cc5bc7cb250a1ef7a6e2d5e926563ddda7f50ee1
MuratOzte <mo2553481@gmail.com>
2024-11-28 17:11:47 +0300

    Add unique constraint to email field and implement email existence check in student registration API

--- src/app/api/(auth)/register/student/route.js
+++ src/app/api/(auth)/register/student/route.js
@@ -1,8 +1,36 @@
 import { NextResponse } from 'next/server';
 
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export async function POST(req, res) {
     const { name, surname, univercity, department, email, password } =
         await req.json();
+
+    const ifExist = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (ifExist) {
+        return NextResponse.json({
+            message: 'This email already exist',
+            status: 400,
+        });
+    }
+
+    const student = await prisma.user.create({
+        data: {
+            name,
+            surname,
+            univercity,
+            department,
+            email,
+            password,
+        },
+    });
 
     return NextResponse.json({
         message: 'student register successful',
