477796661df81d1549f157e41fd5af361579fa78
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-07 00:47:09 +0300

    community add first

---
+++ src/app/(site)/register/community/page.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+// import CustomStepper from '@/app/components/Register/CustomStepper/CustomStepper';
+import CustomStepper from '@/app/components/Register/community/CustomStepper/newCustomStepper';
+import Inputs1 from '@/app/components/Register/community/Pages/page1';
+import Inputs2 from '@/app/components/Register/community/Pages/page2';
+import Inputs3 from '@/app/components/Register/community/Pages/page3';
+
+import logo from '@/assets/logo/logo.png';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
+import AuthBackground from '@/app/components/common/authBackground';
+
+const Register = () => {
+    const step = useSelector((state) => state.register.step);
+
+    return (
+        <div className="w-full h-screen justify-center flex items-center flex-col relative overflow-hidden">
+            <AuthBackground />
+            <div className="w-5/12 h-[550px] bg-white shadow-xl rounded-xl flex justify-center flex-col items-center my-8 z-50 pb-6 px-4">
+                <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: '40%' }}
+                    transition={{ duration: 0.5 }}
+                    className="relative h-[120px] flex justify-center items-center text-center origin-left overflow-hidden"
+                >
+                    <Image
+                        src={logo}
+                        alt="logo"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </motion.div>
+                <CustomStepper />
+                {step === 1 && <Inputs1 />}
+                {step === 2 && <Inputs2 />}
+                {step === 3 && <Inputs3 />}
+            </div>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/app/components/Register/community/CustomStepper/newCustomStepper.jsx
@@ -0,0 +1,186 @@
+'use client';
+// hooks
+import { styled } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { animate, motion } from 'framer-motion'; // Import framer-motion
+// material UI components
+import {
+    Stack,
+    Stepper,
+    Step,
+    StepLabel,
+    StepConnector,
+    stepConnectorClasses,
+} from '@mui/material';
+// icons
+import { FiLogIn } from 'react-icons/fi';
+import { MdAccountCircle } from 'react-icons/md';
+import { MdSchool } from 'react-icons/md';
+import registerSlice from '@/store/Slices/RegisterSlice';
+import { useEffect, useState } from 'react';
+
+const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 22,
+        left: 'calc(-50% + 24px)',
+        right: 'calc(50% + 24px)',
+        zIndex: 1,
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            backgroundColor: '#ccc',
+        },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            backgroundColor: '#4583BD',
+        },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+        height: 3,
+        border: 0,
+        backgroundColor: '#ccc',
+        borderRadius: 1,
+    },
+}));
+
+const ColorlibStepIconRoot = styled(motion.div)(({ theme, ownerState }) => ({
+    backgroundColor:
+        theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 50,
+    height: 50,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...(ownerState.active && {
+        backgroundColor: '#ccc',
+    }),
+    ...(ownerState.completed && {
+        backgroundColor: '#1976d2',
+    }),
+}));
+
+function ColorlibStepIcon(props) {
+    const { active, completed, className } = props;
+
+    const icons = {
+        1: <MdAccountCircle />,
+        2: <MdSchool />,
+        3: <FiLogIn />,
+    };
+
+    return (
+        <ColorlibStepIconRoot
+            ownerState={{ completed, active }}
+            className={className}
+            initial={{ scale: 0 }} // Initial scale for animation
+            animate={{ scale: 1 }} // Scale to 1 for animation
+            transition={{ duration: 0.3 }} // Adjust duration as needed
+        >
+            {icons[String(props.icon)]}
+        </ColorlibStepIconRoot>
+    );
+}
+
+const CustomStepper = () => {
+    const dispatch = useDispatch();
+    const [animatedIcon, setAnimatedIcon] = useState(1);
+
+    const activeStep = useSelector((state) => state.register.step);
+
+    useEffect(() => {
+        setAnimatedIcon(activeStep);
+    }, [activeStep]);
+
+    return (
+        <Stack sx={{ width: '100%' }}>
+            <Stepper
+                sx={{
+                    userSelect: 'none',
+                }}
+                alternativeLabel
+                activeStep={activeStep}
+                connector={
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{
+                            duration: 0.5,
+                            delay: 0.8,
+                        }}
+                    >
+                        <ColorlibConnector />
+                    </motion.div>
+                }
+            >
+                <Step
+                    className={`${
+                        animatedIcon == 1 ? 'animate-scaleUp duration-500' : ''
+                    } z-50`}
+                >
+                    <StepLabel StepIconComponent={ColorlibStepIcon}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{
+                                opacity: activeStep >= 0 ? 1 : 0,
+                                scale: 1,
+                            }}
+                            transition={{
+                                duration: 0.6,
+                            }}
+                        >
+                            Kominite bilgileri
+                        </motion.div>
+                    </StepLabel>
+                </Step>
+                <Step
+                    className={`${
+                        animatedIcon == 2 ? 'animate-scaleUp duration-500' : ''
+                    } z-50`}
+                >
+                    <StepLabel StepIconComponent={ColorlibStepIcon}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{
+                                opacity: activeStep >= 1 ? 1 : 0,
+                                scale: 1,
+                            }}
+                            transition={{
+                                duration: 0.5,
+                                delay: 0.2,
+                            }}
+                        >
+                            Üniversite Bilgileri
+                        </motion.div>
+                    </StepLabel>
+                </Step>
+                <Step
+                    className={`${
+                        animatedIcon == 3 ? 'animate-scaleUp duration-500' : ''
+                    } z-50`}
+                >
+                    <StepLabel StepIconComponent={ColorlibStepIcon}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{
+                                opacity: activeStep >= 1 ? 1 : 0,
+                                scale: 1,
+                            }}
+                            transition={{
+                                duration: 0.5,
+                                delay: 0.4,
+                            }}
+                        >
+                            Giriş Bilgileri
+                        </motion.div>
+                    </StepLabel>
+                </Step>
+            </Stepper>
+        </Stack>
+    );
+};
+
+export default CustomStepper;
