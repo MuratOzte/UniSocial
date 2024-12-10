e7987d923cfa2774ed2ecb75b9a1f747d9b5cd41
MuratOzte <mo2553481@gmail.com>
2024-12-10 03:21:17 +0300

    Add community and event models, enhance Post and User models, and implement image upload functionality

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -1,5 +1,40 @@
+'use client'
+import { useEffect, useState } from "react";
+
 const Main = () => {
-    return <div></div>;
+    const [file, setFile] = useState(null);
+
+    function convertImageToBase64(file) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            console.log(event.target.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    useEffect(() => {
+        console.log(file);
+    }, [file]);
+
+    function handleFileChange(event) {
+        const file = event.target.files[0];
+        setFile(file);
+        convertImageToBase64(file);
+    }
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+
+            {file && (
+                <img
+                    src={URL.createObjectURL(file)}
+                    alt="Preview"
+                    style={{ width: '100px', height: '100px' }}
+                />
+            )}
+        </div>
+    );
 };
 
 export default Main;
