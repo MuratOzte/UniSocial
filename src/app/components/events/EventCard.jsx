9fc123fe4a79598b3c4cb422daff91bc3c74f2b0
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-18 02:52:17 +0300

    joining event funcitonalty

--- src/app/components/events/EventCard.jsx
+++ src/app/components/events/EventCard.jsx
@@ -1,68 +1,75 @@
-import { timeAgo } from '@/util/timeService';
-import React from 'react';
+import { timeAgo } from "@/util/timeService";
+import React from "react";
 
 const EventCard = ({ event }) => {
-    if (!event) return null;
+  if (!event) return null;
 
-    const communityName = event.community
-        ? event.community.name
-        : 'Unknown Community';
-    const communityProfilePicture = event.community
-        ? event.community.profilePicture
-        : '';
+  const communityName = event.community
+    ? event.community.name
+    : "Unknown Community";
+  const communityProfilePicture = event.community
+    ? event.community.profilePicture
+    : "";
 
-    const handleJoin = () => {
-        console.log(event.id);
-        //emrebura join-event apisine istek atcan bodyde eventId:event.id olmalı token olmalı
-    };
+  const handleJoin = async () => {
+    console.log(event.id);
 
-    return (
-        <div className="flex items-center bg-gray-100 border border-gray-300 rounded-lg shadow-lg p-6 space-x-6 min-w-[450px]">
-            <div className="flex-1 flex flex-col gap-5 w-full">
-                <div className="flex justify-between w-full">
-                    <h3 className="text-xl font-semibold text-gray-800">
-                        {event.title}
-                    </h3>
+    const token = localStorage.getItem("token");
+    const response = await fetch("http://localhost:3000/api/join-event", {
+      method: "POST",
+      body: JSON.stringify({
+        eventId: event.id,
+      }),
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
-                    <div className="flex items-end gap-2">
-                        <p className="text-sm text-gray-600">
-                            {event.date} - {event.time} | {event.location}
-                        </p>
-                        <p className="text-sm text-gray-600">
-                            Price: {event.price}{' '}
-                            {event.price > 0 ? '₺' : 'Free'}
-                        </p>
-                    </div>
-                </div>
-                <p className="text-sm text-gray-700">
-                    Community: {communityName}
-                </p>
-                <p className="text-sm text-gray-700">{event.description}</p>
+  return (
+    <div className="flex items-center bg-gray-100 border border-gray-300 rounded-lg shadow-lg p-6 space-x-6 min-w-[450px]">
+      <div className="flex-1 flex flex-col gap-5 w-full">
+        <div className="flex justify-between w-full">
+          <h3 className="text-xl font-semibold text-gray-800">{event.title}</h3>
 
-                <div className="flex justify-between">
-                    <p className="text-sm text-gray-600 mt-2">
-                        {timeAgo(event.createdAt)}
-                    </p>
-                    <button
-                        onClick={handleJoin}
-                        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded min-w-32"
-                    >
-                        Join
-                    </button>
-                </div>
-            </div>
-
-            {event.image && (
-                <div className="flex-shrink-0">
-                    <img
-                        src={event.image}
-                        alt={event.title}
-                        className="w-48 h-32 object-cover rounded-lg border border-gray-300"
-                    />
-                </div>
-            )}
+          <div className="flex items-end gap-2">
+            <p className="text-sm text-gray-600">
+              {event.date} - {event.time} | {event.location}
+            </p>
+            <p className="text-sm text-gray-600">
+              Price: {event.price} {event.price > 0 ? "₺" : "Free"}
+            </p>
+          </div>
         </div>
-    );
+        <p className="text-sm text-gray-700">Community: {communityName}</p>
+        <p className="text-sm text-gray-700">{event.description}</p>
+
+        <div className="flex justify-between">
+          <p className="text-sm text-gray-600 mt-2">
+            {timeAgo(event.createdAt)}
+          </p>
+          <button
+            onClick={handleJoin}
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded min-w-32"
+          >
+            Join
+          </button>
+        </div>
+      </div>
+
+      {event.image && (
+        <div className="flex-shrink-0">
+          <img
+            src={event.image}
+            alt={event.title}
+            className="w-48 h-32 object-cover rounded-lg border border-gray-300"
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EventCard;
