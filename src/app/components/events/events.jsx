5ebed9f563b6d70db1553e496e1a83d839f89e00
MuratOzte <mo2553481@gmail.com>
2024-12-17 17:48:21 +0300

    fetchEvents fn added

--- src/app/components/events/events.jsx
+++ src/app/components/events/events.jsx
@@ -4,6 +4,26 @@ import Event from './events';
 const Events = () => {
     const [events, setEvents] = useState(null);
 
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const response = await fetch(
+                'http://localhost:3000/api/get-all-events',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${localStorage.getItem(
+                            'token'
+                        )}`,
+                    },
+                }
+            );
+            const buffer = await response.json();
+            console.log(buffer);
+        };
+        fetchEvents();
+    }, []);
+
     return (
         events && events.map((event) => <Event key={event.id} event={event} />)
     );
