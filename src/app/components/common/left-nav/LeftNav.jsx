442392b8b09c052493f7d174877e1fa776a54728
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-19 19:31:48 +0300

    left nav send request added

--- src/app/components/common/left-nav/LeftNav.jsx
+++ src/app/components/common/left-nav/LeftNav.jsx
@@ -1,40 +1,63 @@
-import About from './About';
-import Buttons from './Buttons';
-import ImageSection from './ImageSection';
-import Numbers from './Numbers';
+import { useEffect } from "react";
+import About from "./About";
+import Buttons from "./Buttons";
+import ImageSection from "./ImageSection";
+import Numbers from "./Numbers";
 
 const LeftNav = () => {
+  //emre burda http://localhost:3000/api/get-left-bar-info adresine token ile birlikte get request at kanka
+  //dönen response'u burda kullan
+  //böyle bir response döner profile picture null ise react iconsdan account gibi bir icon koyabilirsin
 
-    //emre burda http://localhost:3000/api/get-left-bar-info adresine token ile birlikte get request at kanka
-    //dönen response'u burda kullan
-    //böyle bir response döner profile picture null ise react iconsdan account gibi bir icon koyabilirsin
+  // {
+  //     "message": "User data retrieved successfully",
+  //     "userData": {
+  //       "profilePicture": null,
+  //       "name": "admin",
+  //       "surname": "admin",
+  //       "univercity": "admin",
+  //       "department": "admin",
+  //       "totalPosts": 4,
+  //       "totalFollowers": 0,
+  //       "totalFollowing": 0
+  //     }
+  //   }
 
-    // {
-    //     "message": "User data retrieved successfully",
-    //     "userData": {
-    //       "profilePicture": null,
-    //       "name": "admin",
-    //       "surname": "admin",
-    //       "univercity": "admin",
-    //       "department": "admin",
-    //       "totalPosts": 4,
-    //       "totalFollowers": 0,
-    //       "totalFollowing": 0
-    //     }
-    //   }
+  useEffect(() => {
+    const LeftNavSendRequest = async () => {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        "http://localhost:3000/api/get-left-bar-info",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    };
 
-    return (
-        <div className="w-[350px] rounded-md mt-6 ml-4">
-            <ImageSection />
-            <About
-                department={'Bilgisayar Mühendisliği'}
-                name={'Murat Öztürk'}
-                university={'Karadeniz Teknik Üniversitesi'}
-            />
-            <Numbers follower={123} followings={456} post={789} />
-            <Buttons />
-        </div>
-    );
+    try {
+      LeftNavSendRequest();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <div className="w-[350px] rounded-md mt-6 ml-4">
+      <ImageSection />
+      <About
+        department={"Bilgisayar Mühendisliği"}
+        name={"Murat Öztürk"}
+        university={"Karadeniz Teknik Üniversitesi"}
+      />
+      <Numbers follower={123} followings={456} post={789} />
+      <Buttons />
+    </div>
+  );
 };
 
 export default LeftNav;
