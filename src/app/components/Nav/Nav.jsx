d8a6522a1c51ac183108cb93376a5e46043e2fc6
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-20 03:51:19 +0300

    linkin park

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -1,31 +1,37 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { FaUserCircle } from 'react-icons/fa';
-import { IoSettingsSharp } from 'react-icons/io5';
-import { TbMessages } from 'react-icons/tb';
-
-import Icons from './Icons';
+import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+import { TbMessages } from "react-icons/tb";
+import Link from "next/link";
+import Icons from "./Icons";
 
 const Nav = () => {
-    return (
-        <nav className="flex items-center justify-between px-12 py-2 bg-[#191a1f] shadow-md">
-            <div className="flex items-center h-[80px]">
-                <Image
-                    src={require('@/assets/logo/logo.png')}
-                    alt="UniSocial"
-                    width={200}
-                    height={80}
-                    className="object-cover object-center"
-                />
-            </div>
-            <div className="flex items-center gap-4">
-                <Icons title="Messages" icon={<TbMessages size={20} />} />
-                <Icons title="Settings" icon={<IoSettingsSharp size={20} />} />
-                <Icons title="Account" icon={<FaUserCircle size={20} />} />
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between px-12 py-2 bg-[#191a1f] shadow-md">
+      <div className="flex items-center h-[80px]">
+        <Link href={"/feed"}>
+          <Image
+            src={require("@/assets/logo/logo.png")}
+            alt="UniSocial"
+            width={200}
+            height={80}
+            className="object-cover object-center"
+          />
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href={"/messages"}>
+          <Icons title="Messages" icon={<TbMessages size={20} />} />
+        </Link>
+        <Icons title="Settings" icon={<IoSettingsSharp size={20} />} />
+        <Link href={"/profile"}>
+          <Icons title="Account" icon={<FaUserCircle size={20} />} />
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
