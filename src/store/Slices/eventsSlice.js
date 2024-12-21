5d2c2a1c70024dc032fb0d032510f7d03a7b1390
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-22 02:28:27 +0300

    clubs katıl button

--- src/store/Slices/eventsSlice.js
+++ src/store/Slices/eventsSlice.js
@@ -3,11 +3,17 @@ import { createSlice } from '@reduxjs/toolkit';
 const eventSlice=createSlice({
     name: 'event',
     initialState:{
-        SelectedDate:"2024-12-17"
+        SelectedDate:"2024-12-17",
+        //community
+        isCommunityJoined:"",
+
     },
     reducers:{
         SelectedDateChangeHandler(state,action){
             state.SelectedDate=action.payload
+        },
+        isCommunityJoinedChangeHandler(state,action){
+            state.isCommunityJoined=action.payload;
         }
     },
 
