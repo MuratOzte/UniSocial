990310eda5f2a29542b12dd9382caf81d5248fb5
MuratOzte <mo2553481@gmail.com>
2024-12-23 23:57:07 +0300

    feat: update SearchedUsers component styles and image sizes for better visibility

--- src/app/components/Nav/SearchedUsers.jsx
+++ src/app/components/Nav/SearchedUsers.jsx
@@ -5,17 +5,17 @@ const SearchedUsers = ({ profilePicture, name, surname }) => {
     if (!profilePicture && !name && !surname) return null;
 
     return (
-        <div>
+        <div className='flex gap-4 my-4 text-xl bg-gray-500 text-gray-200 px-4 py-2 items-center cursor-pointer rounded-md' >
             {profilePicture ? (
                 <Image
                     src={profilePicture}
                     alt={name}
-                    width={30}
-                    height={30}
-                    className="rounded-full"
+                    width={50}
+                    height={50}
+                    className="rounded-full border border-gray-300"
                 />
             ) : (
-                <FaUserCircle size={30} />
+                <FaUserCircle size={50} />
             )}
             <p>
                 {name} {'  '}
