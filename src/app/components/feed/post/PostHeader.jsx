86f48b273c01c4e9645bd8cdc1535509c7db8258
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-23 23:32:48 +0300

    post options isyour post fucntionalty added

--- src/app/components/feed/post/PostHeader.jsx
+++ src/app/components/feed/post/PostHeader.jsx
@@ -1,55 +1,54 @@
-import { Tooltip } from '@mui/material';
-import { PiStudent } from 'react-icons/pi';
-import { TbSchool } from 'react-icons/tb';
-import { VscAccount } from 'react-icons/vsc';
-import PostOptions from './PostOptions';
-import { PiStudentBold } from 'react-icons/pi';
-import { FaPeopleRoof } from 'react-icons/fa6';
+import { Tooltip } from "@mui/material";
+import { PiStudent } from "react-icons/pi";
+import { TbSchool } from "react-icons/tb";
+import { VscAccount } from "react-icons/vsc";
+import PostOptions from "./PostOptions";
+import { PiStudentBold } from "react-icons/pi";
+import { FaPeopleRoof } from "react-icons/fa6";
 
 const PostHeader = ({ post, time, isTeacher }) => {
-    const isCommunity = isTeacher === undefined;
+  const isCommunity = isTeacher === undefined;
+  console.log(post);
 
-    //emre burada postOptions componentini post kullanın ise göster post propunda bunla ilgili bir veri var isYourPost gibi
+  return (
+    <div className="flex items-center mb-4">
+      {!post.author.profilePicture && (
+        <VscAccount className="w-8 h-8 rounded-full mr-3" />
+      )}
+      {post.author.profilePicture && (
+        <img
+          src={post.author.profilePicture}
+          alt={`${post.author.name}'s avatar`}
+          className="w-12 h-12 rounded-full mr-3"
+        />
+      )}
 
-    return (
-        <div className="flex items-center mb-4">
-            {!post.author.profilePicture && (
-                <VscAccount className="w-8 h-8 rounded-full mr-3" />
+      <div className="w-full">
+        <div className="text-sm text-gray-400 flex flex-row items-center justify-between w-full">
+          <div className="flex">
+            <p className="font-semibold">{post.author.name}</p>
+            {isTeacher && (
+              <Tooltip title="Teacher" arrow>
+                <TbSchool className="text-lg text-blue-500 ml-1" />
+              </Tooltip>
             )}
-            {post.author.profilePicture && (
-                <img
-                    src={post.author.profilePicture}
-                    alt={`${post.author.name}'s avatar`}
-                    className="w-12 h-12 rounded-full mr-3"
-                />
+            {!isTeacher && !isCommunity && (
+              <Tooltip title="Student" arrow>
+                <PiStudent className="text-lg text-blue-500 ml-1" />
+              </Tooltip>
             )}
-
-            <div className="w-full">
-                <div className="text-sm text-gray-400 flex flex-row items-center justify-between w-full">
-                    <div className="flex">
-                        <p className="font-semibold">{post.author.name}</p>
-                        {isTeacher && (
-                            <Tooltip title="Teacher" arrow>
-                                <TbSchool className="text-lg text-blue-500 ml-1" />
-                            </Tooltip>
-                        )}
-                        {!isTeacher && !isCommunity && (
-                            <Tooltip title="Student" arrow>
-                                <PiStudent className="text-lg text-blue-500 ml-1" />
-                            </Tooltip>
-                        )}
-                        {isCommunity && (
-                            <Tooltip title="Community" arrow>
-                                <FaPeopleRoof className="text-lg text-blue-500 ml-1" />
-                            </Tooltip>
-                        )}
-                    </div>
-                    <PostOptions post={post} />
-                </div>
-                <p className="text-xs text-gray-500">{time}</p>
-            </div>
+            {isCommunity && (
+              <Tooltip title="Community" arrow>
+                <FaPeopleRoof className="text-lg text-blue-500 ml-1" />
+              </Tooltip>
+            )}
+          </div>
+          {post.isYourPost && <PostOptions post={post} />}
         </div>
-    );
+        <p className="text-xs text-gray-500">{time}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PostHeader;
