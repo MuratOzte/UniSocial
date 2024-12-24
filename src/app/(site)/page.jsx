4309dcfb0a622e065e12edeb91b1d9906ee3ab28
MuratOzte <mo2553481@gmail.com>
2024-12-24 06:48:17 +0300

    refactor: replace logo with panda image and update no post found message layout

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -1,36 +1,14 @@
 'use client';
-import slogo from '@/assets/logo/slogo.png';
+import Panda from '@/assets/panda.jpeg';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
 const Main = () => {
-    const [isTriggered, setIsTriggered] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => setIsTriggered(true), 300); // Delay the trigger for a smoother effect
-    }, []);
-
     return (
-        <div className="flex items-center justify-center bg-fff">
-            <span
-                className={`text-transparent justify-end flex bg-clip-text bg-gradient-to-r from-purple-500 via-pink-600 to-red-500 origin-left overflow-hidden transition-all duration-[3000ms] ease-in-out ${
-                    isTriggered ? 'w-[150px]' : 'w-0'
-                } text-6xl`} 
-                style={{ whiteSpace: 'nowrap' }}
-            >
-                Uni
-            </span>
-
-            <Image src={slogo} alt="s" className="h-20 w-16 mx-2" />
-
-            <span
-                className={`text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-600 to-red-500 origin-left inline-block overflow-hidden transition-all duration-[3000ms] ease-in-out ${
-                    isTriggered ? 'w-[150px]' : 'w-0'
-                } text-6xl`} 
-                style={{ display: 'inline-block', whiteSpace: 'nowrap' }}
-            >
-                ocial
-            </span>
+        <div className='flex w-fit px-2 rounded-md bg-gray-400 items-center' >
+            <Image src={Panda} alt="Panda" width={200} height={200} className='mix-blend-color-burn' />
+            <div className='text-center' >
+                <p className='text-2xl text-gray-900' >No Post Found</p>
+                <p className='text-lg text-gray-700' >You can share the first post</p>
+            </div>
         </div>
     );
 };
