ce775c98cec6870a12ceea7eca9cc19539c0a04b
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-12-25 06:11:29 +0300

    userPosts added

--- src/app/(site)/profile/page.jsx
+++ src/app/(site)/profile/page.jsx
@@ -1,32 +1,40 @@
-'use client';
-import Loading from '@/app/components/common/Loading';
-import Nav from '@/app/components/Nav/Nav';
-import ProfileAbout from '@/app/components/Profile/ProfileAbout';
-import ProfileHeader from '@/app/components/Profile/ProfileHeader';
-import ProfileLinks from '@/app/components/Profile/ProfileLinks';
-import ProfilePosts from '@/app/components/Profile/ProfilePosts';
-import useLeftNav from '@/hooks/useLeftNav';
+"use client";
+import Loading from "@/app/components/common/Loading";
+import Nav from "@/app/components/Nav/Nav";
+import ProfileAbout from "@/app/components/Profile/ProfileAbout";
+import ProfileHeader from "@/app/components/Profile/ProfileHeader";
+import ProfileLinks from "@/app/components/Profile/ProfileLinks";
+import ProfilePosts from "@/app/components/Profile/ProfilePosts";
+import useLeftNav from "@/hooks/useLeftNav";
+import { useProfilePosts } from "@/hooks/useProfilePosts";
 
 const Page = () => {
-  const { uidata, error, isLoading } = useLeftNav();
-if(!uidata){
+  const { error, isValidating, posts, refreshPosts } = useProfilePosts();
+  const { uidata} = useLeftNav();
+  if(!posts){
     return <Loading/>
-}
-    return (
-        <>
-            <Nav />
-            <div className="w-full">
-                <ProfileHeader  
-                    uidata={uidata}
-                />
-            </div>
-            <div className="flex justify-between mt-8 mx-8">
-                <ProfileAbout />
-                <ProfilePosts />
-                <ProfileLinks />
-            </div>
-        </>
-    );
+  }
+  if (!uidata) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <Nav />
+      <div className="w-full">
+        <ProfileHeader uidata={uidata} />
+      </div>
+      <div className="flex justify-between mt-8 mx-8">
+        <ProfileAbout />
+        <ProfilePosts
+        error={error}
+        isValidating={isValidating}
+        posts={posts}
+        refreshPosts={refreshPosts}
+         />
+        <ProfileLinks />
+      </div>
+    </>
+  );
 };
 
 export default Page;
