9e62e32eeda950464b5d651815794f9938e2e4f8
MuratOzte <mo2553481@gmail.com>
2024-12-26 05:57:09 +0300

    style: update main page layout with logo and loading spinner for improved branding

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -1,13 +1,28 @@
 'use client';
 import gif from '@/assets/loadingSpinnerGif.gif';
+import logo from '@/assets/logo/logo.png';
 import Image from 'next/image';
+
 const Main = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="flex justify-center items-center text-gray-200 text-7xl">
-                <span>Uni</span>
-                <Image src={gif} width={120} height={120} alt="gif" />
-                <span>ocial</span>
+        <div className="flex justify-center items-center h-screen w-full bg-main1">
+            <div className="flex justify-center items-center">
+                {/* Logo için sadece sol tarafını gösterme */}
+                <div className="relative w-[400px] h-[120px] overflow-hidden">
+                    <Image
+                        src={logo}
+                        alt="logo"
+                        fill // `layout="fill"` yerine `fill` kullanımı
+                        style={{
+                            objectFit: 'cover',
+                            objectPosition: 'left center',
+                        }} // Tailwind yerine inline CSS
+                    />
+                    <div className="absolute bg-red-500 w-[250px] h-32 right-[15px]" />
+                    <Image src={gif} width={120} height={120} alt="gif" className='absolute right-[160px] z-40' />
+                </div>
+
+                {/* Spinner Gif */}
             </div>
         </div>
     );
