b588417dfb073eb5f40359fdb756e49a8f196dfd
zehrademirr <zehraademirr52@gmail.com>
2024-12-30 21:00:29 +0300

    posts page updated

--- src/app/components/feed/post/posts.jsx
+++ src/app/components/feed/post/posts.jsx
@@ -1,45 +1,45 @@
-'use client';
-import { usePosts } from '@/hooks/useFetchPosts';
-import Post from './post';
-import Loading from '../../common/Loading';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import OptimisticPost from './OptimisticPost';
-import Panda from '../../common/Panda';
+"use client";
+import { usePosts } from "@/hooks/useFetchPosts";
+import Post from "./post";
+import Loading from "../../common/Loading";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import OptimisticPost from "./OptimisticPost";
+import Panda from "../../common/Panda";
+
+import ButtonLoading from "../../common/ButtonLoading";
 
 const Posts = () => {
-    const feed = useSelector((state) => state.feed);
+  const feed = useSelector((state) => state.feed);
 
-    const [token, setToken] = useState('');
-    useEffect(() => {
-        setToken(localStorage.getItem('token'));
-    }, []);
-    const { posts, error, isValidating } = usePosts(token);
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
+  const { posts, error, isValidating } = usePosts(token);
 
-    if (isValidating && !posts.length) {
-        return (
-            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
-                <Loading />
-            </div>
-        );
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    return posts.length > 0 ? (
-        <div className='mt-4'>
-            {
-                feed.optimisticPost.isVisible && (
-                    <OptimisticPost />
-                )
-            }
-            {posts.map((post) => <Post key={post.id} post={post} />)}
-        </div>
-    ) : (
-        <Panda />
+  if (isValidating && !posts.length) {
+    return (
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <Loading />
+      </div>
     );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return posts.length > 0 ? (
+    <div className="mt-4">
+      {feed.optimisticPost.isVisible && <OptimisticPost />}
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  ) : (
+    <Panda />
+  );
 };
 
 export default Posts;
