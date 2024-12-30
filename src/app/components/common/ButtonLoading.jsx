f562a05457b5761efd7cf0063298d630a6ad06d4
zehrademirr <zehraademirr52@gmail.com>
2024-12-30 22:00:07 +0300

    follower followings added

--- src/app/components/common/ButtonLoading.jsx
+++ src/app/components/common/ButtonLoading.jsx
@@ -1,6 +1,6 @@
 const ButtonLoading = () => {
   return (
-    <div role="status">
+    <div role="status" className="w-full flex justify-center item-center" >
       <svg
         aria-hidden="true"
         className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
